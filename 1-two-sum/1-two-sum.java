1f1284fb88b72e67c958aeb8fea54528a7bc38bb
MIN JI KIM <52990629+mingg123@users.noreply.github.com>
2022-03-02 01:12:18 +0900

    Time: 808 ms (5.06%), Space: 46.3 MB (8.90%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,15 +1,23 @@
+import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int [] result = new int[2];
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < nums.length; i++) {
-            for(int j = i+1; j < nums.length; j++) {
-                if(i!=j && nums[i] + nums[j] == target) {
+            list.add(nums[i]);
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            int temp = target-nums[i];
+            if(list.contains(temp)) {
+                int index = list.indexOf(temp);
+                if(i!= index) {
                     result[0] = i;
-                    result[1] = j;
-                    break;
+                    result[1] = index;
                 }
             }
         }
+        Arrays.sort(result);
         return result;
     }
 }
