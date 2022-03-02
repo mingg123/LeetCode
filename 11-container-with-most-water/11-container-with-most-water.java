72f98257d17a531c843165e52ab975d115abb5ee
MIN JI KIM <52990629+mingg123@users.noreply.github.com>
2022-03-02 22:54:59 +0900

    Time: 4 ms (72.20%), Space: 81.9 MB (5.95%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int start = 0;
+        int end = height.length-1;
+        while(start< end) {
+            max = Math.max((end-start)* (Math.min(height[start], height[end])), max);
+            if(height[start] < height[end]) {
+                start++;
+            } else {
+                end --;
+            }
+        }
+        return max;
+        
+    }
+}
