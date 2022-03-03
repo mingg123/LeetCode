1e886ebd36dbed00730d1f0683b06342b7fb4a1d
MIN JI KIM <52990629+mingg123@users.noreply.github.com>
2022-03-03 13:23:36 +0900

    Time: 17 ms (15.62%), Space: 45.5 MB (35.82%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,36 +1,44 @@
 class Solution {
+    public static int cal(char c) {
+        int val = 0;
+        switch(c) {
+            case 'I' :
+            val = 1;
+            break;
+            case 'V' :
+            val = 5;
+            break;
+            case 'X' :
+            val = 10;
+            break;
+            case 'L' :
+            val = 50;
+            break;
+            case 'C' :
+            val = 100;
+            break;
+            case 'D' :
+            val = 500;
+            break;
+            case 'M' :
+            val = 1000;
+            break;
+        }
+        return val;
+    }
     public int romanToInt(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
         int result = 0;
-        map.put("I",  1);
-        map.put("IV",  4);
-        map.put("V",  5);
-        map.put("IX",  9);
-        map.put("X",  10);
-        map.put("XL",  40);
-        map.put("L",  50);
-        map.put("XC",  90);
-        map.put("C",  100);
-        map.put("CD",  400);
-        map.put("D",  500);
-        map.put("CM",  900);
-        map.put("M",  1000);
-        
-        while(s.length() >= 1) {
-            String copys = s;
-            String temp = "";
-            if(copys.length() > 1)
-            temp = copys.substring(0,2);
-            String token = copys.substring(0, 1);
-            if(map.containsKey(temp)) {
-                result += map.get(temp);
-                s = s.replaceFirst(temp, "");
+        for(int i = 0; i < s.length()-1; i++) {
+            int now = cal(s.charAt(i));
+            int next = cal(s.charAt(i+1));
+            if(now < next){
+                result += - now;
             } else {
-                result += map.get(token);
-                s = s.replaceFirst(token, "");
+                result += now;
             }
         }
-        // System.out.println(result);
+        result += cal(s.charAt(s.length()-1));
+        System.out.println(result);
         return result;
     }
 }
