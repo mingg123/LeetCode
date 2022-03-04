a0b7dac3c2c1880fa5154b438b0a907a621eeb31
MIN JI KIM <52990629+mingg123@users.noreply.github.com>
2022-03-04 17:57:05 +0900

    Time: 932 ms (28.79%), Space: 42.9 MB (49.40%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        System.out.println(haystack.indexOf(needle));
+        int result = haystack.indexOf(needle);
+        return result;
+    }
+}
