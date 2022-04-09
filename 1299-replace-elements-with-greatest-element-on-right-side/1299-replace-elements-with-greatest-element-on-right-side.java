919621ca720341caa9b87ce53a3cad733c1e0465
MIN JI KIM <52990629+mingg123@users.noreply.github.com>
2022-04-09 12:06:43 +0900

    Time: 1 ms (99.79%), Space: 44.6 MB (83.13%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int [] result = new int[arr.length];
-        for(int i = 0; i < arr.length-1; i++) {
-            int max = arr[i+1];
-            for(int j = i+1; j < arr.length; j++) {
-                if(max < arr[j]) {
-                    max = arr[j];
-                }
-            }
-            result[i] = max;
+        int max = -1;
+        for(int i = arr.length -1; i >=0; i--) {
+            int v = arr[i];
+            arr[i] = max;
+            max = Math.max(v, max);
         }
-        result[arr.length-1] = -1;
-        return result;
+        return arr;
     }
 }
