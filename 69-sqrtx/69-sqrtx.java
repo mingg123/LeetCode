6e59fe97ca6af6c5c209b59e2ff2301f2c20cf5d
MIN JI KIM <52990629+mingg123@users.noreply.github.com>
2022-04-12 14:47:58 +0900

    Time: 3 ms (30.06%), Space: 41.8 MB (21.29%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+        int lo = 1; 
+        int hi = x;
+        while(lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if(mid <=x / mid && (mid+1) > x / (mid+1)) {
+                return mid;
+            } else if(mid<=x / mid) {
+                lo = mid+1;
+            } else {
+                hi = mid-1;
+            }
+        }
+        return lo;
+    }
+}
